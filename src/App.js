afa053a0fffa32b4a110231edff7c2e016245ece
kyeongrok <hydena03@gmail.com>
2024-03-24 23:39:31 +0900

    last commit

--- src/App.js
+++ src/App.js
@@ -1,77 +1,64 @@
-import { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherBox from './component/WeatherBox';
 import WeatherButton from './component/WeatherButton';
 import ClipLoader from "react-spinners/ClipLoader";
 
-
-//1. 앱이 실행되면 위치기반 날씨 확인 가능.
-//2. 도시, 섭씨, 화씨 날씨상태
-//3. 5개의 버튼(1개의 현재위치, 4개의 다른도시)
-//4. 도시버튼을 클릭할때마다 도시별 날씨정보 확인가능
-//5. 현재위치 버튼을 누르면 다시 현재의치 기반의 날씨로 돌아옴.
-//6. 데이터를 들고 오는 동안 로딩스피너 표시.
 function App() {
-
   const [weather, setWeather] = useState(null);
-  const[city,setCity]=useState('')
-  const [loading, setLoading]=useState(false)
-  const cities=['paris','new york','tokyo','seoul']
-  const getCurrentLocation=()=>{
-    navigator.geolocation.getCurrentPosition((position)=>{
-let lat = position.coords.latitude;
-let lon = position.coords.longitude;
-getWeatherByCurrentLocation(lat, lon)
+  const [city, setCity] = useState('');
+  const [loading, setLoading] = useState(false);
+  const cities = ['paris', 'new york', 'tokyo', 'seoul'];
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeatherByCurrentLocation(lat, lon);
     });
   };
 
-const getWeatherByCurrentLocation = async(lat, lon)=>{
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e36d45f52b07a959c9dc024c0ae70387&units=metric`
-setLoading(true)
-  let response = await fetch(url)
-let data = await response.json();
-setWeather(data)
-setLoading(false)
-};
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e36d45f52b07a959c9dc024c0ae70387&units=metric`;
+    setLoading(true);
+    let response = await fetch(url);
+    let data = await response.json();
+    setWeather(data);
+    setLoading(false);
+  };
 
-const getWeatherByCity= async()=>{
-  let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e36d45f52b07a959c9dc024c0ae70387&units=metric`
-setLoading(true)
-let response = await fetch(url)
-let data = await response.json()
-setWeather(data)
-setLoading(false)
- }
+  const getWeatherByCity = async () => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e36d45f52b07a959c9dc024c0ae70387&units=metric`;
+    setLoading(true);
+    let response = await fetch(url);
+    let data = await response.json();
+    setWeather(data);
+    setLoading(false);
+  };
 
-  useEffect(()=> {
-    if(city===""){
-      getCurrentLocation()
-    }else{
-      getWeatherByCity()}
-
-
-  },[city])
-
-
-//   useEffect(()=>{
-// getWeatherByCity()
-//   },[city])
-
+  useEffect(() => {
+    if (city === "") {
+      getCurrentLocation();
+    } else {
+      getWeatherByCity();
+    }
+  }, [city]);
 
   return (
     <div>
-      {loading? (
+      {loading ? (
         <div className="container">
-      <ClipLoader color="#222222" loading={loading} size={150}/>
-      </div>
+          <ClipLoader color="#222222" loading={loading} size={150}/>
+        </div>
       ) : (
-      <div className="container">
-<WeatherBox weather={weather}/>
-<WeatherButton cities={cities} setCity={setCity}/>
-</div>
-  )}
-  </div>
+        <div className="container">
+          <WeatherBox weather={weather}/>
+          <WeatherButton cities={cities} selectedCity={city} setCity={setCity}/>
+        </div>
+      )}
+    </div>
   );
-      }
+}
+
 export default App;
--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const WeatherButton = ({ cities, setCity }) => {
+const WeatherButton = ({ cities, setCity, selectedCity }) => {
   console.log("cities?", cities);
+
   return (
     <div>
-      <Button variant="light">Current Location</Button>{' '}
+
+      <Button
+        style={{ backgroundColor: selectedCity === '' ? '#222222' : 'transparent' }}
+        onClick={() => setCity('')}
+        active={!selectedCity}
+      >
+        Current Location
+      </Button>{' '}
+
       {cities.map((item, index) => (
         <Button
-          variant="warning"
+          style={{ backgroundColor: selectedCity === item ? '#222222' : 'transparent' }}
           key={index}
-          onClick={() => setCity(item)}>
+          onClick={() => setCity(item)}
+          active={selectedCity === item}
+        >
           {item}
         </Button>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default WeatherButton
+export default WeatherButton;
